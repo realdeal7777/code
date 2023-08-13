37893c72606d31f0c77fcdd5e3e080cb821e7a05
realdeal7777 <118578852+realdeal7777@users.noreply.github.com>
2023-08-13 15:46:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let power = prompt('what is the strongest ability in this game');
+
+console.log(power);
+
+alert('this game was made by bicmac');
---
+++ to do.markdown
@@ -0,0 +1,1 @@
+Make a quick RPG to learn more about js and webgl
